79e4be363982f58dcb49b91c68587d2d1240badf
Dranov220805 <duongthanhlong220805@gmail.com>
2026-03-11 23:23:09 +0700

    update project structure

---
+++ shared/LTC.Shared.Hosting.Microservices/ApplicationExceptionFilterAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LTC.Shared.Hosting.Microservices
+{
+    internal class ApplicationExceptionFilterAttribute
+    {
+    }
+}
