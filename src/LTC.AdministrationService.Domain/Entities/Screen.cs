a4150e35d81eab9106faab8b2a5c00b18a76e8dc
Dranov220805 <duongthanhlong220805@gmail.com>
2026-04-10 20:05:09 +0700

    update database schema

--- src/LTC.AdministrationService.Domain/Entities/Screen.cs
+++ src/LTC.AdministrationService.Domain/Entities/Screen.cs
@@ -12,6 +12,7 @@ namespace LTC.AdministrationService.Entities
         public string? ScreenType { get; set; }
         public string? SeatLayout { get; set; }
         public int SeatCount { get; set; }
+        public string? Status { get; set;}
         public DateTime? CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
     }
